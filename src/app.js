eff7ca676f181ec661da4e981922814a18c88a5e
Caricati <thales@caricati.com.br>
2015-08-21 12:58:00 -0300

    Implementing first test, for controller and scope

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('app', []);
 
 app.controller('TodoController', function($scope){
-  // angular scope
+  
 });
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,21 @@
-var assert = chai.assert;
+var expect = chai.expect;
 
 describe('Todo App', function(){
-  describe('Must contain a TodoController', function(){
-    
+
+  var scope, control;
+
+  beforeEach(angular.mock.module('app'));
+  beforeEach(inject(function($rootScope, $controller){
+    scope = $rootScope.$new();
+    control = $controller('TodoController', { $scope: scope });
+  }));
+
+  it('Must contain a TodoController', function(){
+    expect(control).to.not.equal(undefined);
   });
+
+  it('Must contain a number scope ($scope.number) equals to 3', function(){
+    expect(scope.number).to.equal(3);
+  });
+
 });
