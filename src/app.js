50cf183842fd672eac31e96d9f9897d45e5e071b
Caricati <thales@caricati.com.br>
2015-08-25 17:22:52 -0300

    #editTodo() needs more specifications

--- src/app.js
+++ src/app.js
@@ -1,23 +1,19 @@
 var app = angular.module('app', []);
 
 app.controller('TodoController', function($scope){
-  var todoList = [];
+	$scope.todos = [];
 
-  $scope.getTodoList = function() {
-    return todoList;
-  };
+	$scope.addTodo = function(title, completed){
+		if (typeof title !== 'string' || typeof completed !== 'boolean')
+			throw new Error('Invalid parameters.');
 
-  $scope.addTodo = function(title, completed){
-    if (typeof title !== 'string' || typeof completed !== 'boolean')
-      throw new Error('Invalid parameters.');
+		$scope.todos.push({
+			title: title,
+			completed: completed
+		});
+	};
 
-    todoList.push({
-      title: title,
-      completed: completed
-    });
-  };
-
-  $scope.removeTodo = function(todo){
-    return !!todoList.splice(todoList.indexOf(todo), 1).length;
-  };
+	$scope.removeTodo = function(todo){
+		return !!$scope.todos.splice($scope.todos.indexOf(todo), 1).length;
+	};
 });
--- tests/app.test.js
+++ tests/app.test.js
@@ -2,67 +2,90 @@ var expect = chai.expect;
 
 describe('Todo App', function(){
 
-  describe('#TodoController', function(){
-    var scope, control;
+	describe('#TodoController', function(){
+		var scope, control;
 
-    beforeEach(angular.mock.module('app'));
-    beforeEach(inject(function($rootScope, $controller){
-      scope = $rootScope.$new();
-      control = $controller('TodoController', { $scope: scope });
-    }));
+		beforeEach(angular.mock.module('app'));
+		beforeEach(inject(function($rootScope, $controller){
+			scope = $rootScope.$new();
+			control = $controller('TodoController', { $scope: scope });
+		}));
 
-    it('Must contain a TodoController.', function(){
-      expect(control).to.exist;
-    });
+		it('Must contain a TodoController.', function(){
+			expect(control).to.exist;
+		});
 
-    it('#todoList: should be an array.', function(){
-      expect(scope.getTodoList()).instanceof(Array);
-    });
+		it('#todoList: should be an array.', function(){
+			expect(scope.todos).instanceof(Array);
+		});
 
-    it('#addTodo: Should insert todo on todoList.', function(){
-      var lengAfter = 0;
-      var lengBefore = scope.getTodoList().length;
-      scope.addTodo('My todo test', false);
-      lengAfter = scope.getTodoList().length;
-      expect(lengAfter - lengBefore).to.equal(1); // great! its added
-    });
+		it('#addTodo: Should insert todo on todoList.', function(){
+			var lengAfter = 0;
+			var lengBefore = scope.todos.length;
+			scope.addTodo('My todo test', false);
+			lengAfter = scope.todos.length;
+			expect(lengAfter - lengBefore).to.equal(1); // great! its added
+		});
 
-    // needs id, title, completed
-    it('#addTodo: Should validate the parameters.', function(){
-      var addTodo = scope.addTodo;
-      expect(addTodo.bind(addTodo, {}, false)).to.throw('Invalid parameters.');
-      expect(addTodo.bind(addTodo, false, 'false')).to.throw('Invalid parameters.');
-      expect(addTodo.bind(addTodo, 'false', 'false')).to.throw('Invalid parameters.');
-      expect(addTodo.bind(addTodo, 1, true)).to.throw('Invalid parameters.');
-      expect(addTodo.bind(addTodo, 'Test title', 1)).to.throw('Invalid parameters.');
-      expect(addTodo.bind(addTodo, [], true)).to.throw('Invalid parameters.');
-      expect(addTodo.bind(addTodo, [], [true])).to.throw('Invalid parameters.');
-    });
+		// needs id, title, completed
+		it('#addTodo: Should validate the parameters.', function(){
+			var addTodo = scope.addTodo;
+			expect(addTodo.bind(addTodo, {}, false)).to.throw('Invalid parameters.');
+			expect(addTodo.bind(addTodo, false, 'false')).to.throw('Invalid parameters.');
+			expect(addTodo.bind(addTodo, 'false', 'false')).to.throw('Invalid parameters.');
+			expect(addTodo.bind(addTodo, 1, true)).to.throw('Invalid parameters.');
+			expect(addTodo.bind(addTodo, 'Test title', 1)).to.throw('Invalid parameters.');
+			expect(addTodo.bind(addTodo, [], true)).to.throw('Invalid parameters.');
+			expect(addTodo.bind(addTodo, [], [true])).to.throw('Invalid parameters.');
+		});
 
-    it('#removeTodo: Should remove todo on todoList.', function(){
-      var lengBefore = scope.getTodoList().length;
-      var lengAfter;
-      var todo;
+		it('#removeTodo: Should remove todo on todoList.', function(){
+			var lengBefore = scope.todos.length;
+			var lengAfter;
+			var todo;
 
-      scope.addTodo('Test todo', false);
+			scope.addTodo('Test todo', false);
 
-      lengAfter = scope.getTodoList().length;
-      todo = scope.getTodoList()[lengAfter - 1]; // Last todo added
-      scope.removeTodo(todo);
-      expect(lengBefore - lengAfter).to.equal(-1);
-    });
+			lengAfter = scope.todos.length;
+			todo = scope.todos[lengAfter - 1]; // Last todo added
+			scope.removeTodo(todo);
+			expect(lengBefore - lengAfter).to.equal(-1);
+		});
 
-    it('#removeTodo: Should return true if removed successfull.', function(){
-      scope.addTodo('New Todo', false);
-      var todo = scope.getTodoList()[scope.getTodoList().length - 1];
-      expect(scope.removeTodo(todo)).to.be.true;
-      expect(scope.removeTodo({})).to.be.false;
-      expect(scope.removeTodo(false)).to.be.false;
-      expect(scope.removeTodo(true)).to.be.false;
-      expect(scope.removeTodo(null)).to.be.false;
-      expect(scope.removeTodo(0)).to.be.false;
-    });
+		it('#removeTodo: Should return true if removed successfull.', function(){
+			scope.addTodo('New Todo', false);
+			var todoList = scope.todos;
+			var todo = todoList[todoList.length - 1];
+			expect(scope.removeTodo(todo)).to.be.true;
+			expect(scope.removeTodo({})).to.be.false;
+			expect(scope.removeTodo(false)).to.be.false;
+			expect(scope.removeTodo(true)).to.be.false;
+			expect(scope.removeTodo(null)).to.be.false;
+			expect(scope.removeTodo(0)).to.be.false;
+		});
 
-    //it('#editTodo: Should va')
-  });
+		it('#editTodo: Should edit todo', function(){
+			// I need expecificar it better
+			scope.addTodo('Test todo', false);
+			var todo = scope.todos[0];
+
+			expect(scope.editTodo(todo, {
+				title: 'Edited todo'
+			})).to.be.true;
+
+			expect(scope.editTodo(todo, {
+				completed: true
+			})).to.be.true;
+
+			expect(scope.editTodo(todo, {
+				title: 'Edited todo again',
+				completed: false
+			})).to.be.true;
+		});
+
+		it('#editTodo: Should be validate parameters.', function(){
+			// I need expecificar it better
+		});
+
+	});
 });
