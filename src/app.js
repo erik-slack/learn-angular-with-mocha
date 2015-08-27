3d0dbb06046d18051404117718be5408b2d61434
Caricati <thales@caricati.com.br>
2015-08-27 17:09:23 -0300

    update todo validations, add iframe of tests on ./index.html

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 var app = angular.module('app', []);
 
 app.controller('TodoController', function($scope){
-  $scope.todos = [];
+	$scope.todos = [];
 
-  $scope.addTodo = function(title, completed){
-    if (typeof title !== 'string' || typeof completed !== 'boolean')
-      throw new Error('Invalid parameters.');
+	$scope.addTodo = function(title, completed){
+		if (typeof title !== 'string' || typeof completed !== 'boolean')
+			throw new Error('Invalid parameters.');
 
-    $scope.todos.push({
-      title: title,
-      completed: completed
-    });
-  };
+		$scope.todos.push({
+			title: title,
+			completed: completed
+		});
+	};
 
-  $scope.removeTodo = function(todo){
-    return !!$scope.todos.splice($scope.todos.indexOf(todo), 1).length;
-  };
+	$scope.removeTodo = function(todo){
+		return !!$scope.todos.splice($scope.todos.indexOf(todo), 1).length;
+	};
 });
